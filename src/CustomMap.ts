f51ba903c4fa0dab3c326b3c9a40b4e8b45d0044
ryosukehagiwara <hukurahagi1109@gmail.com>
2020-04-10 15:39:42 +0900

    add simple popup

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -20,12 +20,21 @@ export class CustomMap {
   }
 
   addMarker(mappable: Mappable) {
-    new google.maps.Marker({
+    // マーカー作成
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng,
       },
+    });
+
+    // マーカーのポップアップ
+    marker.addListener("click", () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: "Hi, there!",
+      });
+      infoWindow.open(this.googleMap, marker);
     });
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,5 @@ import { Company } from "./Company";
 const customMap = new CustomMap("map");
 
 customMap.addMarker(new User());
+customMap.addMarker(new User());
 customMap.addMarker(new Company());
