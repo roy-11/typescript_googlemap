c24185d5c178ed8ce8643626728fffb28ad7965b
ryosukehagiwara <hukurahagi1109@gmail.com>
2020-04-11 17:03:17 +0900

    fix bug_global variable google is called before the HTML is loaded

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,12 @@ import { CustomMap } from "./CustomMap";
 import { User } from "./User";
 import { Company } from "./Company";
 
-// カスタム（機能制限）のマップ作成
-const customMap = new CustomMap("map");
+window.onload = function () {
+  // マーカ追加機能と機能制限したカスタムマップ作成
+  const customMap = new CustomMap("map");
 
-customMap.addMarker(new User());
-customMap.addMarker(new User());
-customMap.addMarker(new Company());
+  // マーカーを追加
+  customMap.addMarker(new User());
+  customMap.addMarker(new User());
+  customMap.addMarker(new Company());
+};
